e6733756ccee09d54d99c53e6abde7c1b4e2f983
Katie Hannigan <116304115+kathehann@users.noreply.github.com>
2024-11-08 19:15:41 -0800

    new box methods
    the running method uses input points to create the box (only works for 1), there is a commented method that does it with a manually drawn box given specific points

--- SegmentAnything/SegmentHippoNew.py
+++ SegmentAnything/SegmentHippoNew.py
@@ -1,6 +1,6 @@
 from SAMethods import SAM_Image, recommended_kwargs
 import numpy as np
-im = SAM_Image('Cage5195087-Mouse3RL\\NeuN-s2.tif', **recommended_kwargs)
+im = SAM_Image('Cage5195087-Mouse3RL\\NeuN-s3.tif', **recommended_kwargs)
 import matplotlib.pyplot as plt
 
 def get_mask_center(mask):
@@ -12,24 +12,28 @@ def get_mask_bounds(mask):
     nz = np.nonzero(mask)
     return (np.min(nz[1]), np.min(nz[0]), np.max(nz[1]), np.max(nz[0]))
 
-
 #Example of how to get GCL
 #masks, scores, logits = im.get_best_mask([[6000, 3600], [6000, 3200], [6000, 2500], [6000, 4000]], [1, 1, 0, 0])
 #im.display_masks(masks, scores)
 
-#Get central ventricle
-points = [[7500, 3500]]
+points = [[8500, 3500]]
 labels = [1]
 masks, scores, logits = im.get_best_mask(points, labels)
 vent_x,vent_y = get_mask_center(masks[0])
 vent_box = get_mask_bounds(masks[0])
-points = [[int(vent_box[0]*0.75), 3500]]
-im.display(points=points, labels=labels)
+# center_x, center_y = 9500, 3000
 
-#Scan to the left of the ventricle
-# target_x = int(vent_box[0]*0.75)
-# print(target_x)
-# target_column = im.image[:, target_x]
-# brightness = np.mean(target_column, 1)
-# plt.plot(brightness)
-# plt.show()
+# box_width, box_height = 5000, 6000
+
+# x_min = center_x - box_width // 2
+# y_min = center_y - box_height // 2
+# x_max = center_x + box_width // 2
+# y_max = center_y + box_height // 2
+
+
+# boxes = [[x_min, y_min, x_max, y_max]]
+# points = [[center_x, center_y]]
+# labels = [1]
+x_min, y_min, x_max, y_max = vent_box
+boxes = [[x_min, y_min, x_max, y_max]]
+im.display(labels=labels, boxes=boxes)
