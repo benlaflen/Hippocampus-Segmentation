63549541a53e911152e89270bce1d6ca3aa368e3
Katie Hannigan <116304115+kathehann@users.noreply.github.com>
2024-11-12 09:33:31 -0800

    Uses image dimensions to generate box
    Using the image dimensions seems to better fit the center ventricle box than previous methods, It fits all 6 images fairly well

--- SegmentAnything/SegmentHippoBoxedCenter.py
+++ SegmentAnything/SegmentHippoBoxedCenter.py
@@ -16,24 +16,14 @@ def get_mask_bounds(mask):
 #masks, scores, logits = im.get_best_mask([[6000, 3600], [6000, 3200], [6000, 2500], [6000, 4000]], [1, 1, 0, 0])
 #im.display_masks(masks, scores)
 
-points = [[8500, 3500]]
-labels = [1]
-masks, scores, logits = im.get_best_mask(points, labels)
-vent_x,vent_y = get_mask_center(masks[0])
-vent_box = get_mask_bounds(masks[0])
-# center_x, center_y = 9500, 3000
-
-# box_width, box_height = 5000, 6000
-
-# x_min = center_x - box_width // 2
-# y_min = center_y - box_height // 2
-# x_max = center_x + box_width // 2
-# y_max = center_y + box_height // 2
-
-
-# boxes = [[x_min, y_min, x_max, y_max]]
-# points = [[center_x, center_y]]
-# labels = [1]
-x_min, y_min, x_max, y_max = vent_box
+height, width = im.image.shape[:2]
+center_x, center_y = width // 2, height // 2
+box_width, box_height = 5000, 6000
+x_min = center_x - box_width // 2
+y_min = center_y - box_height // 2
+x_max = center_x + box_width // 2 -800 #center ventrical tends to shift left on image
+y_max = center_y + box_height // 2
 boxes = [[x_min, y_min, x_max, y_max]]
+points = [[center_x, center_y]]
+labels = [1]
 im.display(labels=labels, boxes=boxes)
