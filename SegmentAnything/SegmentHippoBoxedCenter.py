59f390a3002bd269f34d94eb7d018f4fea6912f1
Katie Hannigan <116304115+kathehann@users.noreply.github.com>
2024-12-04 10:19:52 -0800

    Update SegmentHippoBoxedCenter.py
    Testing ways to find a negative point without hardcoding...works well for the degenerate sample

--- SegmentAnything/SegmentHippoBoxedCenter.py
+++ SegmentAnything/SegmentHippoBoxedCenter.py
@@ -1,7 +1,7 @@
 from SAMethods import SAM_Image, recommended_kwargs
 import numpy as np
 import cv2
-path = 'Cage5195087-Mouse3RL\\NeuN-s1.tif'
+path = 'Cage4841876-Mouse3RL\\s3-NeuN.tif'
 #D:\Katie\Hippocampus-Segmentation\Cage4841876-Mouse3RL\\s1-NeuN.tif
 #D:\Katie\Hippocampus-Segmentation\Cage5195087-Mouse3RL\\NeuN-s1.tif
 im = SAM_Image(path, **recommended_kwargs)
@@ -33,12 +33,6 @@ end_x = width - width // center_fraction
 start_y = height // center_fraction
 end_y = height - height // center_fraction
 
-#Adjusting the bounding box to better encompass the center
-# start_x-=200
-# end_x-=200
-# start_y+=400
-# end_y+=400
-
 center_binary = binary_image[start_y:end_y, start_x:end_x]
 distance_transform = cv2.distanceTransform(center_binary, cv2.DIST_L2, 5)
 
@@ -68,8 +62,62 @@ for y in range(distance_transform_copy.shape[0]):
 min_val2, max_val2, min_loc2, max_loc2 = cv2.minMaxLoc(distance_transform_copy)
 second_center_ventricle = (max_loc2[0] + start_x, max_loc2[1] + start_y)
 
-points = [[center_ventricle[0], center_ventricle[1]], [second_center_ventricle[0], second_center_ventricle[1]]]
-labels = [1, 1]
+found_white_point = False
+nearest_white_point = None
+
+# Define a range for search (expand from center ventricle)
+search_radius = 500  # You can adjust this based on your need
+
+# Loop in expanding square pattern starting from the center ventricle
+# for radius in range(1, search_radius):
+#     # Check in all four directions (up, down, left, right) in a square pattern
+#     for dx in range(-radius, radius+1):
+#         for dy in range(-radius, radius+1):
+#             x = center_ventricle[0] + dx
+#             y = center_ventricle[1] + dy
+            
+#             # Check if the point is within the bounds of the image
+#             if 0 <= x < binary_image.shape[1] and 0 <= y < binary_image.shape[0]:
+#                 if binary_image[y, x] == 255:  # Found white point
+#                     nearest_white_point = (x, y)
+#                     found_white_point = True
+#                     break
+#         if found_white_point:
+#             break
+#     if found_white_point:
+#         break
+# found_white_point = False
+# nearest_white_point = None
+
+# # Loop through the entire image to find a white point
+# for y in range(binary_image.shape[0]):
+#     for x in range(binary_image.shape[1]):
+#         if binary_image[y, x] == 225:  # White point in the binary image
+#             nearest_white_point = (x, y)
+#             found_white_point = True
+#             break
+#     if found_white_point:
+#         break
+
+# ventricle_mask = np.zeros_like(binary_image)  # Assuming this is your ventricle mask
+# ventricle_mask[start_y:end_y, start_x:end_x] = center_binary  # Fill ventricle region into mask
+
+# # Invert the ventricle mask to get the outside region
+# outside_mask = cv2.bitwise_not(ventricle_mask)
+
+# # Find white points (255) outside the ventricle region
+# outside_white_points = np.nonzero(outside_mask == 255)
+
+# # Pick the first white point outside the ventricle
+# if len(outside_white_points[0]) > 0:
+#     white_x, white_y = outside_white_points[0][0], outside_white_points[1][0]
+#     nearest_white_point = (white_x, white_y)
+# else:
+#     nearest_white_point = (None, None)
+
+
+points = [[center_ventricle[0], center_ventricle[1]], [second_center_ventricle[0], second_center_ventricle[1]],[center_ventricle[0]+1000, center_ventricle[1]+1000]]
+labels = [1, 1,0]
 
 masks, scores, logits = im.get_best_mask(points, labels)
 
